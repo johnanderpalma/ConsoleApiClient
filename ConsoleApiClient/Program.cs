6fa3427a0d4f4815f0698ffa32677ce92fcf34f5
John Ander Palma Fraga <jpalma@stdmultiopcion.es>
2018-01-03 09:07:33 +0100

    Add project files.

---
+++ ConsoleApiClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApiClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
