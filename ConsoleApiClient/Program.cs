72e567ae10b1d0dfefb9695ba6243d2a3f02b318
John Ander Palma Fraga <jpalma@stdmultiopcion.es>
2018-01-03 09:28:11 +0100

    Initial commit, HttpClient it's working

--- ConsoleApiClient/Program.cs
+++ ConsoleApiClient/Program.cs
@@ -1,12 +1,30 @@
-﻿using System;
-
-namespace ConsoleApiClient
+﻿namespace ConsoleApiClient
 {
+    using System;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("GetAll: ");
+            GetAsync().Wait();
+            Console.ReadLine();
+        }
+
+        private static async Task<string> GetAsync()
+        {
+            string stringResponse = string.Empty;
+
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync("https://dog.ceo/api/breeds/list/all");
+                stringResponse = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(stringResponse);
+            }
+
+            return stringResponse;
         }
     }
 }
